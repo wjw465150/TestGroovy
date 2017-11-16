add7e3fbdde0d94d27814cf18ccf7c9dd22347c2
wjw465150 <wjw465150@gmail.com>
2017-11-16 10:15:52 +0800

    初步添加测试接收WebHooks的消息!

---
+++ src/main/webapp/WEB-INF/groovy/test/gitpush.groovy
@@ -0,0 +1,29 @@
+import org.springframework.web.context.WebApplicationContext
+import org.springframework.web.context.support.WebApplicationContextUtils
+
+import groovy.json.*;
+
+WebApplicationContext appContext = WebApplicationContextUtils.getWebApplicationContext(context)
+
+response.contentType = 'text/plain'
+
+
+
+out << "OK"
+
+System.out.println("request属性:");
+System.out.println("getAuthType( ):${request.getAuthType()}");
+System.out.println("getProtocol( ):${request.getProtocol()}");
+System.out.println("getMethod( ):${request.getMethod()}");
+System.out.println("getScheme( ):${request.getScheme()}");
+System.out.println("getContentType( ):${request.getContentType()}");
+System.out.println("getContentLength( ):${request.getContentLength()}");
+System.out.println("getCharacterEncoding( ):${request.getCharacterEncoding()}");
+System.out.println("getRequestedSessionId( ):${request.getRequestedSessionId()}");
+System.out.println("getContextPath( ):${request.getContextPath()}");
+System.out.println("getServletPath( ):${request.getServletPath()}");
+System.out.println("getPathInfo( ):${request.getPathInfo()}");
+System.out.println("getRequestURI( ):${request.getRequestURI()}");
+System.out.println("getRequestURL( ):${request.getRequestURL()}");
+System.out.println("getQueryString( ):${request.getQueryString()}");
+
